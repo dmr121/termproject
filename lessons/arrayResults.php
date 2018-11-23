eeb32817921c987db0b6b87c195ec594d3e5501d
ath <symphoneee@gmail.com>
2018-11-23 00:24:58 -0500

    updating arrays activity

--- lessons/arrayResults.php
+++ lessons/arrayResults.php
@@ -6,16 +6,14 @@
     $answer2 = $_POST['question2'];
     $answer3 = $_POST['question3'];
     $answer4 = $_POST['question4'];
-    $answer5 = $_POST['question5'];
 
     $score = 0;
     
     if ($answer1 == "B") { $score++; }
-    if ($answer2 == "A") { $score++; }
+    if ($answer2 == "B") { $score++; }
     if ($answer3 == "C") { $score++; }
     if ($answer4 == "D") { $score++; }
-    if ($answer5) { $score++; }
-    
+
     echo "<div id='results'>$score / 5 correct</div>";
     
 ?>
