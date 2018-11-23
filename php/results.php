bda6804cdcf4cab42d999765abb9590786279ee4
Cameron Knaus <cameronknaus@Camerons-MacBook-Pro.local>
2018-11-23 18:15:11 -0500

    Create dedicated php folder

--- php/results.php
+++ php/results.php
@@ -1,7 +1,5 @@
-
-
 <?php
-    
+
     $answer1 = $_POST['question1'];
     $answer2 = $_POST['question2'];
     $answer3 = $_POST['question3'];
@@ -9,12 +7,12 @@
     $answer5 = $_POST['question5'];
 
     $totalScore = 0;
-    
+
     if ($answer1 == "B") { $totalScore++; }
     if ($answer2 == "A") { $totalScore++; }
     if ($answer3 == "C") { $totalScore++; }
     if ($answer4 == "D") { $totalScore++; }
     if ($answer5) { $totalScore++; }
-    
-    echo "<div id='results'>$totalScore / 5 correct</div>";   
+
+    echo "<div id='results'>$totalScore / 5 correct</div>";
 ?>
