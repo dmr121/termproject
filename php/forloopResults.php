9b859db22a049aee9016a0645e6ee47508fe4faf
Cameron Knaus <cameronknaus@Camerons-MacBook-Pro.local>
2018-11-23 19:52:31 -0500

    Finish forloops page and stylize submit button

--- php/forloopResults.php
+++ php/forloopResults.php
@@ -4,14 +4,16 @@
     $answer2 = $_POST['question2'];
     $answer3 = $_POST['question3'];
     $answer4 = $_POST['question4'];
+    $answer5 = $_POST['question5'];
 
     $score = 0;
 
     if ($answer1 == "B") { $score++; }
-    if ($answer2 == "B") { $score++; }
-    if ($answer3 == "C") { $score++; }
+    if ($answer2 == "C") { $score++; }
+    if ($answer3 == "B") { $score++; }
     if ($answer4 == "D") { $score++; }
+    if ($answer5 == "D") { $score++; }
 
-    echo "<div id='results'>$score / 4 correct</div>";
+    echo "<div id='results'>$score / 5 correct</div>";
 
 ?>
